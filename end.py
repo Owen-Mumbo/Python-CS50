1ef4734d19f5e59a5b790a405eb7732fae400bca
Owen Mumbo <1marquee125@gmail.com>
2023-10-30 20:34:32 +0300

    added python and csv files

---
+++ end.py
@@ -0,0 +1,8 @@
+import cowsay
+import pyttsx3
+
+engine = pyttsx3
+this = input("What's this? ")
+cowsay.cow(this)
+engine.say(this)
+engine.runAndWait()
---
+++ gryffindors.py
@@ -0,0 +1,4 @@
+students = ["Ron","Hermione","Harry"]
+    
+for i, student in enumerate(students):
+    print(i + 1, student)
